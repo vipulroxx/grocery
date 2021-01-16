614044e37fbcf1aa855b5d0a16c753828769e972
vipulroxx <vipulsharma936@gmail.com>
2021-01-17 01:30:49 +0530

    Connects Category button to Items Page.
    Works for Flour, Oil and Food Grains Button. This will work as a base for adding all items to individual category.

--- app/src/main/java/com/example/grocery/ui/category/CategoryFragment.java
+++ app/src/main/java/com/example/grocery/ui/category/CategoryFragment.java
@@ -1,28 +1,37 @@
 package com.example.grocery.ui.category;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.grocery.CategoryActivity;
 import com.example.grocery.R;
+import com.example.grocery.flourOilFoodGrainsActivity;
 
 public class CategoryFragment extends Fragment {
 
     private CategoryViewModel categoryViewModel;
+    Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        categoryViewModel =
-                ViewModelProviders.of(this).get(CategoryViewModel.class);
+        categoryViewModel = ViewModelProviders.of(this).get(CategoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_category, container, false);
+        button = root.findViewById(R.id.flourOilFoodGrains);
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), CategoryActivity.class);
+            startActivity(intent);
+        });
 
         return root;
     }
--- app/src/main/java/com/example/grocery/ui/category/CategoryViewModel.java
+++ app/src/main/java/com/example/grocery/ui/category/CategoryViewModel.java
@@ -1,9 +1,17 @@
 package com.example.grocery.ui.category;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class CategoryViewModel extends ViewModel {
+import com.example.grocery.CategoryActivity;
+import com.example.grocery.R;
+import com.example.grocery.flourOilFoodGrainsActivity;
 
+
+public class CategoryViewModel extends ViewModel {
 }
